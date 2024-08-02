38ef1880b5e69b16f33d3ac7e335a05ab20d5a2d
Michael Smith <msmith_trd@unknown3ab659df1ca8.attlocal.net>
2024-08-02 14:55:14 -0500

    formatting

--- components/status_bar.go
+++ components/status_bar.go
@@ -1,53 +1,50 @@
 package components
 
 import (
-	"strings"
-
-	"github.com/charmbracelet/lipgloss"
+    "github.com/charmbracelet/lipgloss"
 )
 
 var (
-	statusNugget = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFFDF5")).
-			Padding(0, 1)
+    statusNugget = lipgloss.NewStyle().
+    Foreground(lipgloss.Color("#FFFDF5")).
+    Padding(0, 1)
 
-	statusBarStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
-			Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
+    statusBarStyle = lipgloss.NewStyle().
+    Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
+    Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
 
-	statusStyle = lipgloss.NewStyle().
-			Inherit(statusBarStyle).
-			Foreground(lipgloss.Color("#FFFDF5")).
-			Background(lipgloss.Color("#FF5F87")).
-			Padding(0, 1).
-			MarginRight(1)
+    statusStyle = lipgloss.NewStyle().
+    Inherit(statusBarStyle).
+    Foreground(lipgloss.Color("#FFFDF5")).
+    Background(lipgloss.Color("#FF5F87")).
+    Padding(0, 1).
+    MarginRight(1)
 
-	encodingStyle = statusNugget.
-			Background(lipgloss.Color("#A550DF")).
-			Align(lipgloss.Right)
+    encodingStyle = statusNugget.
+    Background(lipgloss.Color("#A550DF")).
+    Align(lipgloss.Right)
 
-	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+    statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
 
-	fishCakeStyle = statusNugget.Background(lipgloss.Color("#6124DF"))
+    fishCakeStyle = statusNugget.Background(lipgloss.Color("#6124DF"))
 )
 
-func NewStatusBar(doc *strings.Builder, width int) error {
-	w := lipgloss.Width
+func NewStatusBar(width int) string {
+    w := lipgloss.Width
 
-	statusKey := statusStyle.Render("STATUS")
-	encoding := encodingStyle.Render("UTF-8")
-	fishCake := fishCakeStyle.Render("😱 Squeal")
-	statusVal := statusText.
-		Width(width - w(statusKey) - w(encoding) - w(fishCake)).
-		Render("No Databases Configured")
+    statusKey := statusStyle.Render("STATUS")
+    encoding := encodingStyle.Render("UTF-8")
+    fishCake := fishCakeStyle.Render("😱 Squeal")
+    statusVal := statusText.
+    Width(width - w(statusKey) - w(encoding) - w(fishCake)).
+    Render("No Databases Configured")
 
-	bar := lipgloss.JoinHorizontal(lipgloss.Top,
-		statusKey,
-		statusVal,
-		encoding,
-		fishCake,
-	)
+    bar := lipgloss.JoinHorizontal(lipgloss.Top,
+    statusKey,
+    statusVal,
+    encoding,
+    fishCake,
+)
 
-	doc.WriteString(statusBarStyle.Width(width).Render(bar))
-	return nil
+return statusBarStyle.Width(width).Render(bar)
 }
--- main.go
+++ main.go
@@ -3,163 +3,83 @@ package main
 // This example demonstrates various Lip Gloss style and layout features.
 
 import (
-  "log"
-  "time"
+    "log"
+    "strings"
 
-  tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/bubbles/progress"
-  "github.com/charmbracelet/lipgloss"
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/lipgloss"
+
+    "github.com/therealphatmike/squeal/components"
 )
 
 var (
-
-	// General.
-
-	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
-	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
-	special   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
-
-	divider = lipgloss.NewStyle().
-		SetString("•").
-		Padding(0, 1).
-		Foreground(subtle).
-		String()
-
-	url = lipgloss.NewStyle().Foreground(special).Render
-
-  pageStyle = lipgloss.NewStyle().
-      Border(lipgloss.RoundedBorder()).
-      BorderForeground(lipgloss.Color("#26ff00")).
-			Padding(1, 0).
-			BorderTop(true).
-			BorderLeft(true).
-			BorderRight(true).
-			BorderBottom(true)
-	// Dialog.
-
-	dialogBoxStyle = lipgloss.NewStyle().
-			Border(lipgloss.RoundedBorder()).
-			BorderForeground(lipgloss.Color("#874BFD")).
-			Padding(1, 0).
-			BorderTop(true).
-			BorderLeft(true).
-			BorderRight(true).
-			BorderBottom(true)
-
-	buttonStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFF7DB")).
-			Background(lipgloss.Color("#888B7E")).
-			Padding(0, 3).
-			MarginTop(1)
-
-	activeButtonStyle = buttonStyle.
-				Foreground(lipgloss.Color("#FFF7DB")).
-				Background(lipgloss.Color("#F25D94")).
-				MarginRight(2).
-				Underline(true)
+    pageStyle = lipgloss.NewStyle().
+    Border(lipgloss.RoundedBorder()).
+    BorderForeground(lipgloss.Color("#26ff00")).
+    Padding(1, 0).
+    BorderTop(true).
+    BorderLeft(true).
+    BorderRight(true).
+    BorderBottom(true)
 )
 
 type Model struct {
-  width    int
-  height   int
-  loaded   bool
-  progress progress.Model
+    width  int
+    height int
 }
 
-type tickMsg time.Time
-
 func (m Model) Init() tea.Cmd {
-  return tickCmd()
+    return nil
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-  switch msg := msg.(type) {
-  case tea.WindowSizeMsg:
-    m.width = msg.Width
-    m.height = msg.Height
-  case tea.KeyMsg:
-    switch msg.String() {
-      case "ctrl+c":
-        return m, tea.Quit
+    switch msg := msg.(type) {
+    case tea.WindowSizeMsg:
+        m.width = msg.Width
+        m.height = msg.Height
+    case tea.KeyMsg:
+        switch msg.String() {
+        case "ctrl+c":
+            return m, tea.Quit
+        }
+    default:
+        return m, nil
     }
-  case tickMsg:
-		if m.progress.Percent() == 1.0 {
-		  m.loaded = true
-      return m, nil
-		}
-
-		// Note that you can also use progress.Model.SetPercent to set the
-		// percentage value explicitly, too.
-		cmd := m.progress.IncrPercent(0.25)
-		return m, tea.Batch(tickCmd(), cmd)
-
-	// FrameMsg is sent when the progress bar wants to animate itself
-	case progress.FrameMsg:
-		progressModel, cmd := m.progress.Update(msg)
-		m.progress = progressModel.(progress.Model)
-		return m, cmd
-
-	default:
-		return m, nil
-  }
-  return m, nil
-}
-
-func (m Model) getWelcomeDialog() string {
-    okButton := activeButtonStyle.Render("Hell yeah, brother!")
-		cancelButton := buttonStyle.Render("Naw")
-
-		question := lipgloss.NewStyle().Width(50).Align(lipgloss.Center).Render("Welcom to SQueaL, the TUI database manager")
-		buttons := lipgloss.JoinHorizontal(lipgloss.Top, okButton, cancelButton)
-		ui := lipgloss.JoinVertical(lipgloss.Center, question, buttons)
-
-		dialog := lipgloss.Place(m.width, 9,
-			lipgloss.Center, lipgloss.Center,
-			dialogBoxStyle.Render(ui),
-			lipgloss.WithWhitespaceChars("//"),
-			lipgloss.WithWhitespaceForeground(subtle),
-		) 
-
-  return dialog
+    return m, nil
 }
 
 func (m Model) View() string {
-  content := ""
-  if !m.loaded {
-    content = lipgloss.JoinVertical(lipgloss.Center, m.progress.View())
-  } else {
-    content = m.getWelcomeDialog()
-  }
+    content := strings.Builder{}
+    welcome := components.NewWelcomeDialog(m.width)
+    status := components.NewStatusBar(m.width)
 
-  return lipgloss.Place(
-    m.width,
-    m.height,
-    lipgloss.Center,
-    lipgloss.Center,
-    content,
-  )
+    content.WriteString(lipgloss.Place(
+        m.width,
+        m.height,
+        lipgloss.Center,
+        lipgloss.Center,
+        welcome,
+    ) + "\n\n")
+
+    content.WriteString(lipgloss.JoinHorizontal(
+        lipgloss.Top,
+        status,
+    ))
+
+    return content.String()
 }
 
 func main() {
-  m := Model{
-		progress: progress.New(progress.WithDefaultGradient()),
-    loaded:  false,
-	}
+    m := Model{}
 
-  f, err := tea.LogToFile("debug.log", "debug")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer f.Close()
+    f, err := tea.LogToFile("debug.log", "debug")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
 
-  p := tea.NewProgram(m, tea.WithAltScreen())
-  if _, err := p.Run(); err != nil {
-    log.Fatal(err)
-  }
-}
-
-func tickCmd() tea.Cmd {
-	return tea.Tick(time.Second*1, func(t time.Time) tea.Msg {
-		return tickMsg(t)
-	})
+    p := tea.NewProgram(m, tea.WithAltScreen())
+    if _, err := p.Run(); err != nil {
+        log.Fatal(err)
+    }
 }
