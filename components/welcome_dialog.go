efdc8a1fefd498fadb35c8aa2bb03ac2c40f0bd7
T Michael Smith <leogoesgrr@gmail.com>
2024-08-02 19:59:47 -0500

    one more formatting commit. I think I finally fixed my LSP

--- components/welcome_dialog.go
+++ components/welcome_dialog.go
@@ -1,50 +1,50 @@
 package components
 
 import (
-    "github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss"
 )
 
 var (
-    subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	subtle = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
 
-    dialogBoxStyle = lipgloss.NewStyle().
-        Border(lipgloss.RoundedBorder()).
-        BorderForeground(lipgloss.Color("#874BFD")).
-        Padding(1, 0).
-        BorderTop(true).
-        BorderLeft(true).
-        BorderRight(true).
-        BorderBottom(true)
+	dialogBoxStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#874BFD")).
+			Padding(1, 0).
+			BorderTop(true).
+			BorderLeft(true).
+			BorderRight(true).
+			BorderBottom(true)
 
-    buttonStyle = lipgloss.NewStyle().
-        Foreground(lipgloss.Color("#FFF7DB")).
-        Background(lipgloss.Color("#888B7E")).
-        Padding(0, 3).
-        MarginTop(1)
+	buttonStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFF7DB")).
+			Background(lipgloss.Color("#888B7E")).
+			Padding(0, 3).
+			MarginTop(1)
 
-    activeButtonStyle = buttonStyle.
-        Foreground(lipgloss.Color("#FFF7DB")).
-        Background(lipgloss.Color("#F25D94")).
-        MarginRight(2).
-        Underline(true)
+	activeButtonStyle = buttonStyle.
+				Foreground(lipgloss.Color("#FFF7DB")).
+				Background(lipgloss.Color("#F25D94")).
+				MarginRight(2).
+				Underline(true)
 )
 
 func NewWelcomeDialog(width int) string {
-    okButton := activeButtonStyle.Render("Yes")
-    cancelButton := buttonStyle.Render("No")
+	okButton := activeButtonStyle.Render("Yes")
+	cancelButton := buttonStyle.Render("No")
 
-    welcome := lipgloss.NewStyle().Width(56).Align(lipgloss.Center).Render("Welcom to SQueaL, the TUI database manager")
-    noDatabases := lipgloss.NewStyle().Align(lipgloss.Center).Render("It looks like you don't have any databases configured.")
-    setupPrompt := lipgloss.NewStyle().Align(lipgloss.Center).Render("Would you like to set one up?")
-    buttons := lipgloss.JoinHorizontal(lipgloss.Top, okButton, cancelButton)
-    ui := lipgloss.JoinVertical(lipgloss.Center, welcome, "\n\n", noDatabases, setupPrompt, buttons)
+	welcome := lipgloss.NewStyle().Width(56).Align(lipgloss.Center).Render("Welcom to SQueaL, the TUI database manager")
+	noDatabases := lipgloss.NewStyle().Align(lipgloss.Center).Render("It looks like you don't have any databases configured.")
+	setupPrompt := lipgloss.NewStyle().Align(lipgloss.Center).Render("Would you like to set one up?")
+	buttons := lipgloss.JoinHorizontal(lipgloss.Top, okButton, cancelButton)
+	ui := lipgloss.JoinVertical(lipgloss.Center, welcome, "\n\n", noDatabases, setupPrompt, buttons)
 
-    dialog := lipgloss.Place(width, 14,
-        lipgloss.Center, lipgloss.Center,
-        dialogBoxStyle.Render(ui),
-        lipgloss.WithWhitespaceChars("//"),
-        lipgloss.WithWhitespaceForeground(subtle),
-    ) 
+	dialog := lipgloss.Place(width, 14,
+		lipgloss.Center, lipgloss.Center,
+		dialogBoxStyle.Render(ui),
+		lipgloss.WithWhitespaceChars("//"),
+		lipgloss.WithWhitespaceForeground(subtle),
+	)
 
-    return dialog
+	return dialog
 }
--- main.go
+++ main.go
@@ -3,85 +3,85 @@ package main
 // This example demonstrates various Lip Gloss style and layout features.
 
 import (
-    "log"
-    "strings"
+	"log"
+	"strings"
 
-    tea "github.com/charmbracelet/bubbletea"
-    "github.com/charmbracelet/lipgloss"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 
-    "github.com/therealphatmike/squeal/components"
+	"github.com/therealphatmike/squeal/components"
 )
 
 var (
-    pageStyle = lipgloss.NewStyle().
-        Border(lipgloss.RoundedBorder()).
-        BorderForeground(lipgloss.Color("#26ff00")).
-        Padding(1, 0).
-        BorderTop(true).
-        BorderLeft(true).
-        BorderRight(true).
-        BorderBottom(true)
+	pageStyle = lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#26ff00")).
+		Padding(1, 0).
+		BorderTop(true).
+		BorderLeft(true).
+		BorderRight(true).
+		BorderBottom(true)
 )
 
 type Model struct {
-    width  int
-    height int
+	width  int
+	height int
 }
 
 func (m Model) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
-    case tea.WindowSizeMsg:
-        m.width = msg.Width
-        m.height = msg.Height
-    case tea.KeyMsg:
-        switch msg.String() {
-        case "ctrl+c":
-            return m, tea.Quit
-        }
-    default:
-        return m, nil
-    }
-    return m, nil
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	default:
+		return m, nil
+	}
+	return m, nil
 }
 
 func (m Model) View() string {
-    content := strings.Builder{}
-    welcome := components.NewWelcomeDialog(m.width)
-    quickKeys := components.NewQuickKeys(m.width)
-    status := components.NewStatusBar(m.width)
+	content := strings.Builder{}
+	welcome := components.NewWelcomeDialog(m.width)
+	quickKeys := components.NewQuickKeys(m.width)
+	status := components.NewStatusBar(m.width)
 
-    content.WriteString(lipgloss.Place(
-        m.width,
-        m.height,
-        lipgloss.Center,
-        lipgloss.Center,
-        welcome,
-    ) + "\n\n")
+	content.WriteString(lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		welcome,
+	) + "\n\n")
 
-    content.WriteString(lipgloss.JoinVertical(
-        lipgloss.Left,
-        quickKeys,
-        status,
-    ))
+	content.WriteString(lipgloss.JoinVertical(
+		lipgloss.Left,
+		quickKeys,
+		status,
+	))
 
-    return content.String()
+	return content.String()
 }
 
 func main() {
-    m := Model{}
+	m := Model{}
 
-    f, err := tea.LogToFile("debug.log", "debug")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer f.Close()
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
 
-    p := tea.NewProgram(m, tea.WithAltScreen())
-    if _, err := p.Run(); err != nil {
-        log.Fatal(err)
-    }
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
